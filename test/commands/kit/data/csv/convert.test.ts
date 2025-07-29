8be9005a30d1b7e776cb9ecf2d6c6e74b4b765ec
shota maehana <fpu811@gmail.com>
2025-07-29 21:42:18 +0900

    fix csv convert test

--- test/commands/kit/data/csv/convert.test.ts
+++ test/commands/kit/data/csv/convert.test.ts
@@ -14,7 +14,10 @@ describe('kit data csv convert', () => {
   let writeToStream: any;
   let createReadStream: any;
   let createWriteStream: any;
+  let saveCsv: any;
   let readJson: any;
+  let parseCsv: any;
+  let loadScript: any;
   beforeEach(async () => {
     writeToStream = $$.SANDBOX.fake();
     createReadStream = $$.SANDBOX.fake.returns(
@@ -22,41 +25,54 @@ describe('kit data csv convert', () => {
     );
     createWriteStream = $$.SANDBOX.fake();
     readJson = $$.SANDBOX.fake.returns({ field: 'b' });
+    parseCsv = $$.SANDBOX.stub();
+    loadScript = $$.SANDBOX.stub();
     Command = await esmock(
       '../../../../../src/commands/kit/data/csv/convert.js',
       {
         'fast-csv': { writeToStream },
         'fs-extra': { createReadStream, createWriteStream, readJson },
+        '../../../../../src/utils.js': { parseCsv, loadScript },
       }
     );
+    saveCsv = $$.SANDBOX.stub(Command.prototype, 'saveCsv');
   });
 
   const defaultArgs = ['-i', 'data/input.csv', '-o', 'data/output.csv'];
   it(defaultArgs.join(' '), async () => {
+    parseCsv.resolves(csvRows);
+
     await Command.run(defaultArgs);
-    expect(createReadStream.calledWith('data/input.csv')).to.be.true;
-    expect(createWriteStream.calledWith('data/output.csv')).to.be.true;
-    expect(writeToStream.calledOnce).to.be.true;
-    expect(writeToStream.args[0][1]).to.eql(csvRows);
+    expect(parseCsv.calledOnce).to.be.true;
+    expect(saveCsv.calledWith('data/output.csv')).to.be.true;
+    expect(saveCsv.calledOnce).to.be.true;
+    expect(saveCsv.args[0][1]).to.eql(csvRows);
   });
 
   const mappingArgs = defaultArgs.concat('-m', 'data/mapping.json');
   it(mappingArgs.join(' '), async () => {
+    const mappingResult = [{ field: 'b1' }, { field: 'b2' }];
+    parseCsv.resolves(mappingResult);
+
     await Command.run(mappingArgs);
     expect(readJson.calledOnce).to.be.true;
     expect(readJson.args[0][0]).to.eq('data/mapping.json');
-    expect(writeToStream.args[0][1]).to.eql([{ field: 'b1' }, { field: 'b2' }]);
+    expect(parseCsv.calledOnce).to.be.true;
+    expect(saveCsv.args[0][1]).to.eql(mappingResult);
   });
 
   const convertArgs = defaultArgs.concat('-c', 'data/convert.js');
   it(convertArgs.join(' '), async () => {
-    const loadConverter = $$.SANDBOX.stub(
-      Command.prototype,
-      'loadConverter'
-    ).returns((row: any) => ({ field: row.b.toUpperCase() }));
+    const convertResult = [{ field: 'B1' }, { field: 'B2' }];
+    parseCsv.resolves(convertResult);
+    loadScript.returns({
+      convert: (row: any) => ({ field: row.b.toUpperCase() }),
+    });
+
     await Command.run(convertArgs);
-    expect(loadConverter.calledOnce).to.be.true;
-    expect(loadConverter.args[0][0]).to.eq('data/convert.js');
-    expect(writeToStream.args[0][1]).to.eql([{ field: 'B1' }, { field: 'B2' }]);
+    expect(loadScript.calledOnce).to.be.true;
+    expect(loadScript.args[0][0]).to.eq('data/convert.js');
+    expect(parseCsv.calledOnce).to.be.true;
+    expect(saveCsv.args[0][1]).to.eql(convertResult);
   });
 });
